a02052491f60aec30411237d249f5edf30063310
stBiker999 <103806972+stBiker999@users.noreply.github.com>
2023-10-04 08:13:57 +0000

    a

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div id="app">
+    <!-- メインレイアウトを挿入 -->
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* スタイリングを適用するためのCSSを追加 */
+#app {
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+  color: #333;
+}
+
+/* ページのスタイリングを追加 */
+</style>
